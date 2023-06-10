1a00fc9ba62d8cc8b7dad18f70a7838c66a8cf1e
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-06-10 14:55:04 -0300

    feat: implementação do componente PlanetCard

---
+++ src/components/PlanetCard.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+
+export interface PropPlanetCard {
+  planetName: string;
+  planetImage: any;
+}
+
+export default function PlanetCard(props: PropPlanetCard) {
+  const { planetName, planetImage } = props;
+
+  return (
+    <li data-testid='planet-card'>
+      <p>{planetName}</p>
+      <Image
+        src={planetImage}
+        alt={planetName}
+      />
+    </li>
+  );
+}
