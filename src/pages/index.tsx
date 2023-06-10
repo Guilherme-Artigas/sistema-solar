47817952938725b9c9edeeb98b9609f20acb7448
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-06-10 14:54:37 -0300

    chore: adiciona favicon, e planetData

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
+import Head from 'next/head';
 import Header from '@/components/Header';
 import SolarSystem from '@/components/SolarSystem';
 
 export default function home() {
   return (
-    <>
+    <div>
+      <Head>
+        <title>Sistema Solar</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <Header />
       <SolarSystem />
-    </>
+    </div>
   );
 }
